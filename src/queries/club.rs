a646a3920ad650e76285ba6cc82fdfb5dfb270de
f0reachARR <f0reach@f0reach.me>
2021-02-20 19:11:06 +0900

    Add get club list query

--- src/queries/club.rs
+++ src/queries/club.rs
@@ -3,7 +3,7 @@ use diesel::{prelude::*, r2d2::ConnectionManager};
 use r2d2::Pool;
 
 use crate::{
-    model_resolver::club::ClubWithLevelItem,
+    model_resolver::club::{ClubWithLevelItem, ClubWithMembers},
     models::{Club, User, UserClubRelation},
 };
 
@@ -23,12 +23,38 @@ impl ClubQuery {
             user_club_relation::table
                 .inner_join(club::table)
                 .filter(user_club_relation::user_id.eq(user.id.0))
+                .order(club::created_at.desc())
                 .load::<(UserClubRelation, Club)>(&conn)?
         };
 
         Ok(clubs
             .into_iter()
             .map(|club| ClubWithLevelItem(club.0.level, club.1))
+            .collect())
+    }
+
+    async fn get_clubs<'a>(
+        &self,
+        ctx: &'a Context<'_>,
+        offset: Option<i64>,
+        limit: Option<i64>,
+    ) -> Result<Vec<ClubWithMembers>> {
+        let pool = ctx.data::<Pool<ConnectionManager<MysqlConnection>>>()?;
+        let conn = pool.get()?;
+
+        let clubs = {
+            use crate::models::schema::club;
+
+            club::table
+                .order(club::created_at.desc())
+                .offset(offset.unwrap_or(0))
+                .limit(limit.unwrap_or(10))
+                .load::<Club>(&conn)?
+        };
+
+        Ok(clubs
+            .into_iter()
+            .map(|club| ClubWithMembers(club))
             .collect())
     }
 }
