1a84d7785b3501a9f573e95ebb99f0534ab7114b
f0reachARR <f0reach@f0reach.me>
2021-02-20 20:04:57 +0900

    Add user list query

--- src/queries/user.rs
+++ src/queries/user.rs
@@ -1,6 +1,8 @@
 use async_graphql::{Context, Object, Result};
+use diesel::{prelude::*, r2d2::ConnectionManager};
+use r2d2::Pool;
 
-use crate::models::User;
+use crate::models::{User, UserPermission};
 
 #[derive(Debug, Default)]
 pub struct UserQuery;
@@ -21,5 +23,33 @@ impl UserQuery {
         } else {
             Err(async_graphql::Error::new("Not logged in"))
         }
+    }
+
+    async fn get_users<'a>(
+        &self,
+        ctx: &'a Context<'_>,
+        offset: Option<i64>,
+        limit: Option<i64>,
+        least_permission: Option<UserPermission>,
+    ) -> Result<Vec<User>> {
+        let pool = ctx.data::<Pool<ConnectionManager<MysqlConnection>>>()?;
+        let conn = pool.get()?;
+
+        let users = {
+            use crate::models::schema::user;
+
+            let mut filter = user::table
+                .order(user::id.desc())
+                .offset(offset.unwrap_or(0))
+                .limit(limit.unwrap_or(10))
+                .into_boxed();
+            if let Some(perm) = least_permission {
+                filter = filter.filter(user::permission.ge(perm));
+            }
+
+            filter.get_results::<User>(&conn)?
+        };
+
+        Ok(users)
     }
 }
