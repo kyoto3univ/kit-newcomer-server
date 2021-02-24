70cc7a38919bdebd7873b2ba2896457856e751d9
f0reachARR <f0reach@f0reach.me>
2021-02-24 17:54:28 +0900

    Fix asset mutation

--- src/mutations/mod.rs
+++ src/mutations/mod.rs
@@ -1,14 +1,15 @@
 use async_graphql::MergedObject;
 use twitter::TwitterAuthenticationMutation;
 
+mod asset;
 mod club;
 mod twitter;
 mod user;
-mod asset;
 
 #[derive(MergedObject, Default)]
 pub struct MutationRoot(
     TwitterAuthenticationMutation,
     club::ClubMutation,
     user::UserMutation,
+    asset::AssetMutation,
 );
